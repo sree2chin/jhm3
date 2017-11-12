1ff76719525fe162fb4ccddcc4a99e421c825155
sureify <sureify@gmail.com>
2017-11-12 11:48:50 +0530

    no product changes

--- code/src/components/quote/NoProducts.tsx
+++ code/src/components/quote/NoProducts.tsx
@@ -12,16 +12,18 @@ export default class NoProducts extends React.Component<Props, {}> {
 
   public render() {
     return (
-      <Col sm={12} className="no-product-container">
-        <div className="inner-no-product-container">
-          <Row className="" style={{fontSize: "26px", color: "#999999"}}>
-            No Eligible products
-          </Row>
-          <Row style={{fontSize: "13px", color: "#999999"}}> 
-            We're sorry, we couldn't find any eligible products for you.
-          </Row>
-        </div>
-      </Col>
+      <Row className="no-product-outer-container">
+        <Col sm={12} className="no-product-container">
+          <div className="inner-no-product-container">
+            <Row className="" style={{fontSize: "26px", color: "#999999"}}>
+              No Eligible products
+            </Row>
+            <Row style={{fontSize: "13px", color: "#999999"}}> 
+              We're sorry, we couldn't find any eligible products for you.
+            </Row>
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
