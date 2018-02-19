c79edf4ec5d639753837172334a689d74be5af7c
Durga Vinay <durgavinay333@gmail.com>
2018-02-19 21:03:34 +0530

    gulp prod

--- code/src/components/questions/InstantIdCheckPopup.tsx
+++ code/src/components/questions/InstantIdCheckPopup.tsx
@@ -18,7 +18,10 @@ export default class InstantIdCheckPopup extends React.Component<Props, {}> {
 
     return (
           <Modal className="instant-id-check-popup-container" show={this.props.showInstantIdCheckPopup} onHide={this.props.onCloseModal} bsSize="big">
-                <Modal.Header closeButton>
+                <Modal.Header>
+                  <img style={{float: "right", marginBottom: "25px", width: "24px", height: "24px"}} src={"../images/delete_beneficiary.svg"} onClick={()=>{
+                          this.props.onCloseModal();
+                        }}/>
                 </Modal.Header>
                 <Modal.Body style={{ fontSize: "25px", textAlign: "center"}}>
                     <Row>
@@ -29,9 +32,9 @@ export default class InstantIdCheckPopup extends React.Component<Props, {}> {
                         {this.props.instantIdCheckData && this.props.instantIdCheckData.message_data}
                       </Col>
                       <Col>
-                        <Button className={`c-button-default circular action`} style={{marginLeft: "20px"}} onClick={()=>{
+                        <Button className={`c-button-default circular action`} style={{marginLeft: "15px"}} onClick={()=>{
                             this.props.onCloseModal()
-                            }}>
+                          }}>
                             CLOSE
                         </Button>
                       </Col>
