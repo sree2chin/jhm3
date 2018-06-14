0a5ca899089e0fb9c1b0db1460c0daa67c3d944a
Durga Vinay <durgavinay333@gmail.com>
2018-06-14 17:55:45 +0530

    changes

--- code/services/questions.js
+++ code/services/questions.js
@@ -50,6 +50,13 @@ module.exports = new function() {
       if (!err && res.statusCode == 200) {
         callback(res.statusCode, res.body);
       } else {
+        var responseBody = JSON.parse(JSON.stringify(JSON.parse(res.body)));
+
+        if (responseBody && responseBody.data && responseBody.data.current_document_data) {
+          req.session.envelop_id = responseBody.data.current_document_data.envelop_id;
+          req.session.signature_status = responseBody.data.current_document_data.signature_status;
+          console.log("\n\n\nreq.session.envelop_id: " + req.session.envelop_id + "\n\n\n");
+        }
         callback(res.statusCode, res.body);
       }
     });
