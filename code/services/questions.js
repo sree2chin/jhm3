92387c0af6f89b2767de92234778bf31b9a8df1a
Durga Vinay <durgavinay333@gmail.com>
2018-06-25 21:32:22 +0530

    small change

--- code/services/questions.js
+++ code/services/questions.js
@@ -48,10 +48,16 @@ module.exports = new function() {
   this.postPayment = function(req, callback) {
     ApiService.postPayment(req, function(err, res) {
       if (!err && res.statusCode == 200) {
+        var responseBody = JSON.parse(JSON.stringify(JSON.parse(res.body)));
+        if (responseBody && responseBody.data && responseBody.data.current_document_data) {
+          req.session.envelop_id = responseBody.data.current_document_data.envelop_id;
+          req.session.signature_status = responseBody.data.current_document_data.signature_status;
+          console.log("\n\n\nreq.session.envelop_id: " + req.session.envelop_id + "\n\n\n");
+        }
         callback(res.statusCode, res.body);
       } else {
         var responseBody = JSON.parse(JSON.stringify(JSON.parse(res.body)));
-
+        console.log("\n\n\n responseBody in postPayment: " + JSON.stringify(responseBody) + "\n\n\n");
         if (responseBody && responseBody.data && responseBody.data.current_document_data) {
           req.session.envelop_id = responseBody.data.current_document_data.envelop_id;
           req.session.signature_status = responseBody.data.current_document_data.signature_status;
