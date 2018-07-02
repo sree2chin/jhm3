6a455349cc9a9b4f2415670477d6cbe32258e207
Durga Vinay <durgavinay333@gmail.com>
2018-07-02 22:23:03 +0530

    health pop changes again

--- code/src/components/quote/Rider.tsx
+++ code/src/components/quote/Rider.tsx
@@ -21,7 +21,8 @@ export default class Plan extends React.Component<Props, {}> {
     this.props.selectRider(this.props.rider, this.props.riderKey);
   }
   getPremiumType() {
-    if ( this.props.premium_type.toUpperCase() == "Semi-Annual".toUpperCase()) {
+    return this.props.premium_type;
+    if (this.props.premium_type.toUpperCase() == "Semi-Annual".toUpperCase()) {
       return this.props.premium_type;
     }
     return this.props.premium_type.substring(0, 2).toUpperCase()
@@ -51,18 +52,24 @@ export default class Plan extends React.Component<Props, {}> {
               </Col>
             </Row>
           </Row>
-          {this.props.riderKey != "WP" && <Row className={`text-center quote-product rider-footer-container`} style={{paddingTop: "10px", marginLeft: "0px", height: "62px", fontFamily: "NunitoRegular!important"}} onClick={()=> this.selectRider(rider)}>
+          {this.props.riderKey != "WP" && <Row className={`text-center quote-product rider-footer-container`} style={{paddingTop: "10px", marginLeft: "0px", height: "70px", fontFamily: "NunitoRegular!important"}} onClick={()=> this.selectRider(rider)}>
             <div className="c-coverage-amount" style={{fontSize: "14px", color: "#317dbd"}}>included in policy</div>
             <div className="c-coverage-amount additional-text" style={{fontSize: "14px", color: "#999999"}}>(no additional premium)</div>
           </Row>}
-          {this.props.riderKey == "WP" && <Row className={`text-center quote-product `} style={{paddingTop: "10px", marginLeft: "0px", height: "62px", fontFamily: "NunitoRegular!important", fontSize: "12px"}}>
-            {!this.props.isActive && <Button style={{float: "right", width: "88%", marginRight: "6%", marginTop: "0px"}} className={`c-button-default circular rider-adding-btn ${this.state.riderAdded ? "active" : ""}`} onClick={(){
-                this.selectRider(rider, this.props.riderKey)
-              }}
-            >
-              ADD +${this.props.rider.Premium[this.props.premium_type] ? parseFloat(this.props.rider.Premium[this.props.premium_type].split("$")[1]) : 0}/{this.getPremiumType()}
+          {this.props.riderKey == "WP" && <Row className={`text-center quote-product `} style={{paddingTop: "10px", marginLeft: "0px", height: "70px", fontFamily: "NunitoRegular!important", fontSize: "12px"}}>
+            {!this.props.isActive &&
+              <Button
+                style={{paddingTop: "3px", height: "48px!important", float: "right", width: "88%", marginRight: "6%", marginTop: "0px"}}
+                className={`c-button-default circular rider-adding-btn ${this.state.riderAdded ? "active" : ""}`} onClick={()=>{
+                  this.selectRider(rider, this.props.riderKey)
+                }}
+              >
+              <div>ADD +${this.props.rider.Premium[this.props.premium_type] ? parseFloat(this.props.rider.Premium[this.props.premium_type].split("$")[1]) : 0}</div>
+              <div>{this.getPremiumType()}</div>
             </Button>}
-            {this.props.isActive && <Button style={{float: "right", width: "88%", marginRight: "6%", marginTop: "0px"}} className={`c-button-default circular active rider-adding-btn ${this.state.riderAdded ? "active" : ""}`} onClick={(){
+
+            {this.props.isActive && <Button style={{height: "48px!important", float: "right", width: "88%", marginRight: "6%", marginTop: "0px"}} className={`c-button-default circular active rider-adding-btn ${this.state.riderAdded ? "active" : ""}`}
+              onClick={()=>{
                 this.selectRider(rider, this.props.riderKey)
               }}
             >
